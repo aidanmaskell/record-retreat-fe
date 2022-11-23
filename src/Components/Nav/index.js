371221bf44f3dae009403efcee8fa31ae3433879
aidanmaskell <aidan.maskell@gmail.com>
2022-11-23 14:11:02 +0000

    fixed responsivity on nav

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -4,7 +4,7 @@ import './styles.scss'
 const Nav = () => {
   return (
     <div className='m-0'>
-      <div className='nav row d-flex justify-content-center col-12 col-md-6 offset-md-3'>
+      <div className='nav row d-flex justify-content-center col col-md-6 offset-md-3'>
           <button className='col-4 py-2'><Link className='navLinks'to="/">COLLECTION</Link></button>
           <button className='col-4 py-2'><Link className='navLinks'to="/shuffle">SHUFFLE</Link></button>
           <button className='col-4 py-2'><Link className='navLinks'to="/addrecord">ADD RECORD</Link></button>
