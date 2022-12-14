d2b837a3d7fc889da966e7b43be54d23dc30654f
aidanmaskell <aidan.maskell@gmail.com>
2022-12-14 16:27:57 +0000

    added like button functionality

--- src/Components/LikeButton/index.js
+++ src/Components/LikeButton/index.js
@@ -2,27 +2,27 @@ import './styles.scss'
 
 const LikeButton = ({ likeId }) => {
 
-    // const likeRecord = (likeId) => {
-    //     fetch(`http://127.0.0.1:3000/collection/like/${likeId}`, {
-    //         method: 'PUT'
-    //     })
-    //     .then(data => data.json())
-    //     .then((response) => {
-    //         console.log(response)
-    //         if (response.success === true) {
-    //             console.log('hello')
-    //         }
-    //     })
-    // }
+    const likeRecord = (likeId) => {
+        fetch(`http://127.0.0.1:3000/collection/like/${likeId}`, {
+            method: 'PUT'
+        })
+        .then(data => data.json())
+        .then((response) => {
+            console.log(response)
+            if (response.success === true) {
+                console.log('hello')
+            }
+        })
+    }
 
-    // const handleClick = (likeId) => {
-    //     likeRecord(likeId)
-    // }
+    const handleClick = () => {
+        likeRecord(likeId)
+    }
 
     return (
         <>
             <button 
-                // onClick={handleClick(likeId)} 
+                onClick={handleClick} 
                 id={likeId} className='m-1 like'>Like</button>
         </>
     )
